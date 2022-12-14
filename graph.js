9f29329bc6ae07fc10163f1c3c5053a951997ce7
Karol Dobiczek <k.t.dobiczek@student.tudelft.nl>
2022-12-14 23:19:03 +0100

    add psd graphs

--- graph.js
+++ graph.js
@@ -4,11 +4,11 @@ console.log("Hi!");
 width = 720;
 height = 480;
 
-var margin = {top: 10, right: 30, bottom: 30, left: 60}
+var margin = {top: 20, right: 30, bottom: 30, left: 60}
 
 var size = 100;
 svgs = [];
-for (i = 0; i < 31; i++) {
+for (i = 0; i < 60; i++) {
     var svg = d3.select("body")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -37,52 +37,44 @@ d3.csv("/data/eeg-lab-sample-export.csv", function (data) {
         values: values
     }
 
+    vls = []
+    tms = []
+
     d_row = []
-    for (i = 0; i < 5000; i++) {
+    for (i = 0; i < 1000; i++) {
         d_row.push(
             {
-                time: time[i],
-                values: values[i]
+                time: parseFloat(time[i]),
+                values: parseFloat(values[i])
+            }
+        );
+        vls.push(values[i]);
+        tms.push(time[i])
+    }
+
+    console.log(d_row)
+    console.log(d3.extent(d_row, d => d.time))
+
+    psd = bci.welch(vls, 1/time[1]);
+    console.log(psd);
+
+    p_row = [];
+
+
+    for (i = 0; i < psd.estimates.length; i++) {
+        p_row.push(
+            {
+                time: parseFloat(psd.frequencies[i]),
+                values: Math.log(parseFloat(psd.estimates[i]))
             }
         );
     }
+    console.log(p_row)
 
     // console.log(d3.extent(d_row, function(d) { return d.time; }));
 
-    var x = d3.scaleTime()
-        .domain(d3.extent(d_row, function (d) { return d.time; }))
-        .range([0, width]);
-    svgs[j].append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    extent = d3.extent(d_row, function (d) { return d.values; });
-    range = extent[1] - extent[0];
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain(extent)
-        .range([extent[1]/range*height/2, -extent[0]/range*height/2]);
-    svgs[j].append("g")
-        .call(d3.axisLeft(y));
-
-    // Add the line
-    svgs[j].append("path")
-        .datum(d_row)
-        .attr("fill", "none")
-        .attr("stroke", "rgb(" + (100 + 17 * j) % 255 + "," + (200 + 13 * j) % 255 + "," + (0 + 37 * j) % 255 + ")")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-            .x(d => x(d.time))
-            .y(d => y(d.values))
-        )
-
-    svgs[j].append("text")
-        .attr("x", (width / 2))             
-        .attr("y", 20 - (margin.top / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "16px")  
-        .text(value_name);
+    plot(d_row, svgs, 2*j, value_name);
+    plot(p_row, svgs, 1+2*j, value_name + " PSD");
 
     // console.log(x(values[0]))
 
@@ -91,3 +83,40 @@ d3.csv("/data/eeg-lab-sample-export.csv", function (data) {
 });
 
 // console.log(rows);
+function plot(data, svgs, index, title) {
+    var x = d3.scaleLinear()
+        .domain(d3.extent(data, function (d) { return d.time; }))
+        .range([0, width]);
+    svgs[index].append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    extent = d3.extent(data, function (d) { return d.values; });
+    range = extent[1] - extent[0];
+
+    // Add Y axis
+    console.log(extent)
+    var y = d3.scaleLinear()
+        .domain(extent)
+        .range([height, 10]);
+    svgs[index].append("g")
+        .call(d3.axisLeft(y));
+
+    // Add the line
+    svgs[index].append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "rgb(" + (100 + 17 * j) % 255 + "," + (200 + 13 * j) % 255 + "," + (0 + 157 * j) % 255 + ")")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => x(d.time))
+            .y(d => y(d.values))
+        )
+
+    svgs[index].append("text")
+        .attr("x", (width / 2))             
+        .attr("y", (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px")  
+        .text(title);
+}
